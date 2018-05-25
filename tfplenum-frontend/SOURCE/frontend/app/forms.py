fb169ec032900a0c6f80e8fd3b987fe83f80ad20
Unknown <grant.curell@salientcrgt.com>
2018-05-25 18:37:00 -0500

    Added bootstrap

--- tfplenum-frontend/SOURCE/frontend/app/forms.py
+++ tfplenum-frontend/SOURCE/frontend/app/forms.py
@@ -6,5 +6,11 @@ class InventoryForm(FlaskForm):
     #username = StringField('Username', validators=[DataRequired()])
     #password = PasswordField('Password', validators=[DataRequired()])
     #remember_me = BooleanField('Remember Me')
-    dns_ip = StringField('DNS IP Address', validators=[IPAddress(ipv4=True)])
+    dns_ip = StringField('DNS IP Address', description=
+    "The IP address of the system DNS server. You may define this or it will   \
+     default  to using the master server's management IP. We suggest you leave \
+     it to default  unless you have a specific reason to use a different DNS   \
+     server. Keep in mind  you will need to manually provide all required DNS  \
+     entries on your separate  DNS Server or the kit will break."              \
+     , validators=[IPAddress(ipv4=True)])
     submit = SubmitField('Save Inventory')
--- tfplenum-frontend/SOURCE/frontend/app/views.py
+++ tfplenum-frontend/SOURCE/frontend/app/views.py
@@ -4,19 +4,14 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import InventoryForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    form = InventoryForm()
+    if form.validate_on_submit():
+        flash('Request for inventory file submitted.')
+    return render_template('index.html', title='Configure Inventory', form=form)
 
 @app.route('/about')
 def about():
     return render_template("about.html")
-
-@app.route('/inventory', methods=['GET', 'POST'])
-def inventory():
-    form = InventoryForm()
-    if form.validate_on_submit():
-        flash('Request for inventory file submitted.')
-        return redirect(url_for('index'))
-    return render_template('inventory.html', title='Configure Inventory', form=form)
