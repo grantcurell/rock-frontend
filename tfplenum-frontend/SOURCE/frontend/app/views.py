f8b6eab41b9aa457a00abae126afe2256102b0f8
Unknown <grant.curell@salientcrgt.com>
2018-05-26 16:56:31 -0500

    Bah time for some xcom

--- tfplenum-frontend/SOURCE/frontend/app/views.py
+++ tfplenum-frontend/SOURCE/frontend/app/views.py
@@ -4,6 +4,12 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import InventoryForm
 
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
