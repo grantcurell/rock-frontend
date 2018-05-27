631d714b9733c05f37333a2a5edeba8ed4fd3c1d
Unknown <grant.curell@salientcrgt.com>
2018-05-27 06:40:45 -0500

    Installed jquery

--- tfplenum-frontend/SOURCE/frontend/app/views.py
+++ tfplenum-frontend/SOURCE/frontend/app/views.py
@@ -1,6 +1,6 @@
 # views.py
 
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request, jsonify
 from app import app
 from app.forms import InventoryForm
 
