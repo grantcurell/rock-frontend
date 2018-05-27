1cb9b9c91df27598ecfd41b2c8a8316e522fa786
Unknown <grant.curell@salientcrgt.com>
2018-05-27 12:04:22 -0500

    Started custom functions

---
+++ tfplenum-frontend/SOURCE/frontend/app/static/foozandfuncs/foozandfuncs.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+  $('a#calculate').bind('click', function() {
+    $.getJSON($SCRIPT_ROOT + '/_add_numbers', {
+      a: $('input[name="a"]').val(),
+      b: $('input[name="b"]').val()
+    }, function(data) {
+      $("#result").text(data.result);
+    });
+    return false;
+  });
+});
+
+
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").hide();
+    });
+});
+
+
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
