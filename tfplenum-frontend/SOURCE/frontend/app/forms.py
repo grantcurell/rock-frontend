1391a78631d22d05d44c325fe2294aaf8b9f431b
Unknown <grant.curell@salientcrgt.com>
2018-05-28 13:09:05 -0500

    Fixed valid and invalid feedback

--- tfplenum-frontend/SOURCE/frontend/app/forms.py
+++ tfplenum-frontend/SOURCE/frontend/app/forms.py
@@ -1,18 +1,20 @@
 
-
+# https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5/Constraint_validation
 class Field:
-    def __init__(self, id=None, label=None, description=None, placeholder=None, input_type=None, html5_constraint=None, required=True):
+    def __init__(self, id=None, label=None, description=None, placeholder=None,
+                 input_type=None, html5_constraint=None, required=True, valid_feedback=None,
+                 invalid_feedback=None):
       self.id = id
       self.label = label
       self.description = description
       self.placeholder = placeholder
       self.input_type = input_type
       self.html5_constraint = html5_constraint
+      self.valid_feedback = valid_feedback
+      self.invalid_feedback = invalid_feedback
 
       if required:
           self.required = 'required'
-      else:
-          self.required = ''
 
 class Button(Field, object):
     def __init__(self, button_text=None, **kwargs):
@@ -21,6 +23,17 @@ class Button(Field, object):
 
 class InventoryForm:
 
+  number_of_servers = Button(
+    id = 'num_servers' \
+  , label = 'Number of Servers' \
+  , button_text = 'Submit' \
+  , placeholder = "Enter the number of servers you plan to run in your kit" \
+  , input_type = 'number' \
+  , html5_constraint = 'min=1' \
+  , required = True \
+  , valid_feedback = 'Looks good! Configure your sensors in the \'Server Settings\' section below.' \
+  , invalid_feedback = 'You must have at least one server.')
+
   number_of_sensors = Button(
     id = 'num_sensors' \
   , label = 'Number of Sensors' \
@@ -28,7 +41,9 @@ class InventoryForm:
   , placeholder = "Enter the number of sensors you plan to run in your kit" \
   , input_type = 'number' \
   , html5_constraint = 'min=1' \
-  , required = True)
+  , required = True \
+  , valid_feedback = 'Looks good! Configure your sensors in the \'Sensor Settings\' section below.' \
+  , invalid_feedback = 'You must have at least one sensor.')
 
   dns_ip = Field(
     label = 'DNS IP Address' \
--- tfplenum-frontend/SOURCE/frontend/app/templates/foozandfuncs.js
+++ tfplenum-frontend/SOURCE/frontend/app/templates/foozandfuncs.js
@@ -1,38 +1,3 @@
-/*
-$.validator.setDefaults( {
-	submitHandler: function () {
-		alert( "submitted!" );
-	}
-} );
-
-
-$( document ).ready( function () {
-	$( "#form_sensor" ).validate( {
-		rules: {
-			{{ form.number_of_sensors.id }}: {
-				required: true
-			},
-		},
-		messages: {
-			{{ form.number_of_sensors.id }}: {
-				minlength: "Your username must consist of at least 2 characters"
-			},
-		},
-		errorElement: "div",
-		errorPlacement: function ( error, element ) {
-			// Add the `help-block` class to the error element
-			error.addClass( "invalid-feedback" );
-		  //error.insertAfter( element.next( $(".input-group-append") ));
-		},
-		highlight: function ( element, errorClass, validClass ) {
-			$( element ).parents( $( ".input-group mb-3" ) ).addClass( "is-invalid" ).removeClass( "has-success" );
-		},
-		unhighlight: function (element, errorClass, validClass) {
-			$( element ).parents( $( ".input-group mb-3" ) ).addClass( "has-success" ).removeClass( "has-error" );
-		}
-	} );
-} );
-*/
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (function() {
