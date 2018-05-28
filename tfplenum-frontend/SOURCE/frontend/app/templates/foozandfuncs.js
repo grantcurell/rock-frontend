c4dcc76314db8f2354b31992c3815e17fb4d1c14
Unknown <grant.curell@salientcrgt.com>
2018-05-27 19:58:23 -0500

    Time for more xcom and westworld

--- tfplenum-frontend/SOURCE/frontend/app/templates/foozandfuncs.js
+++ tfplenum-frontend/SOURCE/frontend/app/templates/foozandfuncs.js
@@ -7,13 +7,12 @@ $.validator.setDefaults( {
 $( document ).ready( function () {
 	$( "#form_sensor" ).validate( {
 		rules: {
-			number_of_sensors: {
-				required: true,
-				minlength: 2
+			{{ form.number_of_sensors.id }}: {
+				required: true
 			},
 		},
 		messages: {
-			number_of_sensors: {
+			{{ form.number_of_sensors.id }}: {
 				minlength: "Your username must consist of at least 2 characters"
 			},
 		},
@@ -21,19 +20,13 @@ $( document ).ready( function () {
 		errorPlacement: function ( error, element ) {
 			// Add the `help-block` class to the error element
 			error.addClass( "invalid-feedback" );
-
-			if ( element.prop( "type" ) === "checkbox" ) {
-				error.insertAfter( element.parent( "label" ) );
-			} else {
-        // input-group-append is the class for the button we are appending
-				error.insertAfter( element.next( $(".input-group-append") ));
-			}
+		  error.insertAfter( element.next( $(".input-group-append") ));
 		},
 		highlight: function ( element, errorClass, validClass ) {
-			$( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+			$( element ).parents( $( ".input-group mb-3" ) ).addClass( "has-error" ).removeClass( "has-success" );
 		},
 		unhighlight: function (element, errorClass, validClass) {
-			$( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+			$( element ).parents( $( ".input-group mb-3" ) ).addClass( "has-success" ).removeClass( "has-error" );
 		}
 	} );
 } );
