6a6b562a12de7e9873afca8dd2529a57c8825af7
Unknown <grant.curell@salientcrgt.com>
2018-05-27 19:09:23 -0500

    Error working without wtforms

--- tfplenum-frontend/SOURCE/frontend/app/templates/foozandfuncs.js
+++ tfplenum-frontend/SOURCE/frontend/app/templates/foozandfuncs.js
@@ -1,34 +1,3 @@
-
-$(document).ready(function() {
-  $('a#calculate').bind('click', function() {
-    $.getJSON($SCRIPT_ROOT + '/_add_numbers', {
-      a: $('input[name="a"]').val(),
-      b: $('input[name="b"]').val()
-    }, function(data) {
-      $("#result").text(data.result);
-    });
-    return false;
-  });
-});
-
-
-/*
-$(document).ready(function(){
-    $("#{{ form.number_of_sensors_id }}").click(function(){
-        alert("Value: " + $("#number_of_sensors").val())
-        $('#main').validate({ // initialize the plugin
-            rules: {
-                number_of_sensors: {
-                    required: true,
-                    minlength: 1
-                }
-            }
-        });
-        $("#wrong").html("<b>Hello world!</b>");
-    });
-});
-*/
-
 $.validator.setDefaults( {
 	submitHandler: function () {
 		alert( "submitted!" );
@@ -38,45 +7,15 @@ $.validator.setDefaults( {
 $( document ).ready( function () {
 	$( "#form_sensor" ).validate( {
 		rules: {
-			number_of_sensors: "required",
-			lastname: "required",
-			username: {
+			number_of_sensors: {
 				required: true,
 				minlength: 2
 			},
-			password: {
-				required: true,
-				minlength: 5
-			},
-			confirm_password: {
-				required: true,
-				minlength: 5,
-				equalTo: "#password"
-			},
-			email: {
-				required: true,
-				email: true
-			},
-			agree: "required"
 		},
 		messages: {
-			firstname: "Please enter your firstname",
-			lastname: "Please enter your lastname",
-			username: {
-				required: "Please enter a username",
+			number_of_sensors: {
 				minlength: "Your username must consist of at least 2 characters"
 			},
-			password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long"
-			},
-			confirm_password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long",
-				equalTo: "Please enter the same password as above"
-			},
-			email: "Please enter a valid email address",
-			agree: "Please accept our policy"
 		},
 		errorElement: "div",
 		errorPlacement: function ( error, element ) {
