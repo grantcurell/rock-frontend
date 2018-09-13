9c0f37412975f30f5a90c1049ea0033f53634684
david.navarro <david.navarro@mantech.com>
2018-09-13 16:07:01 +0000

    943 - Fixed a bug where duplicate IPs were not being checked.

--- frontend/src/app/kit-form/kit-form-validation.ts
+++ frontend/src/app/kit-form/kit-form-validation.ts
@@ -4,8 +4,13 @@ import { ServerFormGroup, SensorFormGroup, SensorsFormArray, ServersFormArray } 
 import { GetClusteredStorageValidated, GetElasticSearchValidated, GetSensorResourcesValidated } from './kit-form-globals';
 import { CEPH_DRIVE_MIN_COUNT } from '../frontend-constants';
 
-
-function _validateMasterServer(control: AbstractControl, errors: Array<string>){
+/**
+ * Ensures that the user has selected at least one server as a master.
+ * 
+ * @param control - The KitForm Group
+ * @param errors - An array of strings to display.
+ */
+function _validateMasterServer(control: AbstractControl, errors: Array<string>): void {
     let servers = control.get('servers') as ServersFormArray;
     if (servers != null){
         for (let i = 0; i < servers.length; i++) {
@@ -18,7 +23,13 @@ function _validateMasterServer(control: AbstractControl, errors: Array<string>){
     errors.push("- Master server failed to validate. Did you remember to select a master server? (It's the checkbox that says 'Is Kubernetes master server?')");
 }
 
-function _validateCephDriveCount(control: AbstractControl, errors: Array<string>){
+/**
+ * Ensures that there are enough drives selected for the CEPH cluster configuration.
+ * 
+ * @param control - The KitForm Group
+ * @param errors - An array of strings to display.
+ */
+function _validateCephDriveCount(control: AbstractControl, errors: Array<string>): void {
     let servers = control.get('servers') as ServersFormArray;
     let sensors = control.get('sensors') as SensorsFormArray;
     let ceph_drive_count = 0;
@@ -41,8 +52,14 @@ function _validateCephDriveCount(control: AbstractControl, errors: Array<string>
         errors.push("- Ceph drives failed to validate. You have to have at least two drives in your Ceph cluster!");
     }    
 }
-  
-function _validateMonitorInterfaces(control: AbstractControl, errors: Array<string>){
+ 
+/**
+ * Ensures that at least one interface is selected for each sensor
+ * 
+ * @param control - The KitForm Group
+ * @param errors - An array of strings to display.
+ */
+function _validateMonitorInterfaces(control: AbstractControl, errors: Array<string>): void {
     let sensors = control.get('sensors') as SensorsFormArray;
     let error_message = "- Monitor interfaces failed to validate. You need to select at least one monitor interface on each sensor.";    
 
@@ -59,14 +76,28 @@ function _validateMonitorInterfaces(control: AbstractControl, errors: Array<stri
     }
 }
 
-function _validateKubernetesCIDR(control: AbstractControl, errors: Array<string>){
+
+/**
+ * Ensures the user entered the Kubernetes CIDR value.
+ * 
+ * @param control - The KitForm Group
+ * @param errors - An array of strings to display.
+ */
+function _validateKubernetesCIDR(control: AbstractControl, errors: Array<string>): void {
     let kubernetes = control.get('kubernetes_services_cidr') as HtmlInput;
     if (kubernetes != null && !kubernetes.valid){
         errors.push("- Kubernetes services CIDR failed to validate. Make sure you type in a valid Kubernetes services CIDR under Global Settings.");
     }    
 }
 
-function _validateHomeNet(control: AbstractControl, errors: Array<string>){
+
+/**
+ * Ensures at least home net is filled out.
+ * 
+ * @param control - The KitForm Group
+ * @param errors - An array of strings to display.
+ */
+function _validateHomeNet(control: AbstractControl, errors: Array<string>): void {
     let sensor_resources = control.get('sensor_resources');
     
     if (sensor_resources != null){
@@ -81,7 +112,13 @@ function _validateHomeNet(control: AbstractControl, errors: Array<string>){
     }    
 }
 
-function _validateHosts(control: AbstractControl, errors: Array<string>){
+/**
+ * Validates that the user hit the "Gather Facts" button for each sensor and server.
+ * 
+ * @param control - The KitForm Group
+ * @param errors - An array of strings to display.
+ */
+function _validateHosts(control: AbstractControl, errors: Array<string>): void {
     let servers = control.get('servers') as ServersFormArray;
     let sensors = control.get('sensors') as SensorsFormArray;
     let invalid_host:string = '- Invalid hostname. Did you forget to click "Gather Facts" on one of your sensors or servers?';
@@ -107,7 +144,13 @@ function _validateHosts(control: AbstractControl, errors: Array<string>){
     }
 }
 
-function _validateLogstashReplicas(control: AbstractControl, errors: Array<string>){
+/**
+ * Ensures that logstash replicas is set appropriatley.
+ * 
+ * @param control - The KitForm Group
+ * @param errors - An array of strings to display.
+ */
+function _validateLogstashReplicas(control: AbstractControl, errors: Array<string>): void {
     let servers = control.get('servers') as ServersFormArray;
     let sensors = control.get('sensors') as SensorsFormArray;
     
@@ -139,8 +182,53 @@ function _validateLogstashReplicas(control: AbstractControl, errors: Array<strin
     }
 }
 
-export function ValidateKitInventory(control: AbstractControl){
-    let errors = [];
+/**
+ * Validates IPs on KitForm page to ensure that a user does not have duplicate IPs for both sensors or servers.
+ * 
+ * @param control - The KitForm Group
+ * @param errors - An array of strings to display.
+ */
+function _validateIps(control: AbstractControl, errors: Array<string>): void {
+    let servers = control.get('servers') as ServersFormArray;
+    let sensors = control.get('sensors') as SensorsFormArray;
+    let ips: Array<string> = [];
+
+    if (servers != null) {
+        for (let i = 0; i < servers.length; i++) {
+            let server = servers.at(i) as ServerFormGroup;
+            ips.push(server.host_server.value);
+        }
+    }
+
+    if (sensors != null) {
+        for (let i = 0; i < sensors.length; i++) {
+            let sensor = sensors.at(i) as SensorFormGroup;
+            ips.push(sensor.host_server.value);
+        }
+    }
+
+    for (let i = 0; i < ips.length; i++) {
+        let ipA = ips[i];
+
+        for (let x = (i + 1); x < ips.length; x++) {
+            let ipB = ips[x];
+
+            if (ipA == ipB){
+                errors.push('- Duplicate ips have been detected. Please make sure you do not have duplicate ips in your form.')
+                return;
+            }            
+        }
+    }
+}
+
+/**
+ * The main exported function that performs all the Form Level validation for the KitForm.
+ * 
+ * @param control - The KitForm Group
+ * @returns - A array of error messages if there are errors.
+ */
+export function ValidateKitInventory(control: AbstractControl): { errors: Array<string> } {
+    let errors: Array<string> = [];
 
     _validateMonitorInterfaces(control, errors);
     _validateCephDriveCount(control, errors);
@@ -148,6 +236,7 @@ export function ValidateKitInventory(control: AbstractControl){
     _validateKubernetesCIDR(control, errors);
     _validateHomeNet(control, errors);
     _validateHosts(control, errors);
+    _validateIps(control, errors);
     _validateLogstashReplicas(control, errors);
 
     if (!GetClusteredStorageValidated()){
