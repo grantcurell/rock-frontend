dd972b20343699ca7e04b6fc468cd204e9b2c4b9
root <david.navarro@mantech.com>
2019-02-04 11:59:16 -0600

    1928 - Fixed portal page broken links and removed links that do not need to be displayed.

--- frontend/src/app/portal/portal.component.ts
+++ frontend/src/app/portal/portal.component.ts
@@ -7,24 +7,17 @@ import { PortalService } from '../portal.service';
   styleUrls: ['./portal.component.css']
 })
 export class PortalComponent implements OnInit {  
-  links: Array<{ip: string, dns: string}>;
+  links: Array<{ip: string, dns: string, logins: string}>;
 
   constructor(private portalSrv: PortalService) { 
     this.links = new Array();
   }
 
-  ngOnInit() {    
+
+  ngOnInit() {            
     this.portalSrv.getPortalLinks().subscribe(data => {
-      let portalLinks = data as Array<{ip: string, dns: string}>;
-      for (let link of portalLinks){
-        
-        if (link.dns == "kubernetes-dashboard.lan"){
-          this.links.push({ip: "https://" + link.ip, dns: "https://" + link.dns});
-        } else {
-          this.links.push({ip: "http://" + link.ip, dns: "http://" + link.dns});
-        }
-      }
-      
+      let portalLinks = data as Array<{ip: string, dns: string, logins: string}>;
+      this.links = portalLinks;
     });
   }
 
